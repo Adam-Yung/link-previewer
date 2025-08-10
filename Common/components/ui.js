d21543071939348968b7cd437883c86cf7dd6f5a
Adam Yung <adamyung@gmail.com>
2025-08-10 11:43:00 -0700

    Revert will-change fixes.  The speedup is inconsistent across browsers, resulting in a slow-down on integrated graphics

--- Common/components/ui.js
+++ Common/components/ui.js
@@ -61,7 +61,6 @@ function createPreview(url) {
   container.id = 'link-preview-container';
   container.classList.add(settings.theme);
   container.style.pointerEvents = 'auto';
-  container.style.willChange = "transform, opacity, contents";
   state.container = container;
 
   // Apply size and position from settings.
@@ -137,7 +136,6 @@ function createPreview(url) {
             addressBar.classList.remove('is-loading', 'ocean-blue', 'ocean-orange', 'ocean-magenta');
           }
         }, Math.max(0, 1000 - remainingTime));
-        container.style.willChange = '';
       };
       container.appendChild(img);
       addressBar.addEventListener('mousedown', (e) => initDrag(e, container, img));
@@ -158,7 +156,6 @@ function createPreview(url) {
                     addressBar.classList.remove('is-loading', 'ocean-blue', 'ocean-orange', 'ocean-magenta');
                   }
                 }, Math.max(0, 1000 - remainingTime));
-                container.style.willChange = '';
               }
               checkForIframeReady(iframe, shadowRoot);
             } else {
@@ -286,7 +283,6 @@ function createPreview(url) {
 
   document.addEventListener('keydown', handleEsc);
 
-  disableParentVisibility(true);
   toggleDisableParentPage(isInCenterStage());
 }
 
@@ -297,7 +293,7 @@ function closePreview() {
   if (!state.isPreviewing) return;
 
   toggleDisableParentPage(false);
-  disableParentVisibility(false);
+
   const previewHost = document.getElementById('link-preview-host');
   const pageOverlay = document.getElementById('link-preview-page-overlay');
 
